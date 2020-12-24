739ac8293e8d1d7741f38571c160b4ee9a5763dc
Joan <69289383+clh-cod@users.noreply.github.com>
2020-12-24 10:20:24 +0800

    Delete docker-image.sh

--- spring3-mvc-maven-xml-hello-world/docker-image.sh
+++
@@ -1,6 +0,0 @@
-docker stop tomcat
-docker rm tomcat
-docker rmi 192.168.1.179:8002/tomcat:0.0.0
-docker build -t 192.168.1.179:8002/tomcat:0.0.0 .
-docker push 192.168.1.179:8002/tomcat:0.0.0
-docker run -idt -p 28080:8080 --name tomcat 192.168.1.179:8002/tomcat:0.0.0
